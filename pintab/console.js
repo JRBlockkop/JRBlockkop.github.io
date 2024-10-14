79a22114cb7d311bb54c5cec2833464a4c64d8b3
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-14 19:55:32 +0200

    Update console.js

--- pintab/console.js
+++ pintab/console.js
@@ -2,7 +2,13 @@ const Pin = {
     console:{
         status:"off",
     },
+    User:{
+        name:null;
+    },
     status:function (){
+        if(JSON.parse(document.getElementById("ifrdata").contentDocument.body.innerHTML).username!=''){
+            Pin.User.name=JSON.parse(document.getElementById("ifrdata").contentDocument.body.innerHTML).username
+        }
         if(Pin.User.name != ''){
             document.getElementById("obenbar").innerHTML = '<div class="Account"><div class="icon-color red">'+Pin.User.name[0]+'</div>'+Pin.User.name+'</div>';
         }
