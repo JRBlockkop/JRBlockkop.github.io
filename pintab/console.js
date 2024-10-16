daaf18c6c8315c7a6529f696e0e39556a38c2307
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-16 21:02:40 +0200

    Update console.js

--- pintab/console.js
+++ pintab/console.js
@@ -24,5 +24,5 @@ function DataUserSetN(name){
     document.getElementById("ifrdata").contentDocument.location.href = "https://jrblockkop.github.io/pintab/data.html?username="+name
 }
 function LoginData(username,password){
-    fetch("https://JRBlockkop.github.io/pintab/db.json").then(x=>console.log(x.read()))
+    fetch("https://JRBlockkop.github.io/pintab/db.json").then(x=>console.log(x.text()))
 }
