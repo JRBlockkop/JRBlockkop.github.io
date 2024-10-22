40bae127c671cef58c1c747c6f0960422043e091
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-22 20:26:08 +0200

    Create search.js

---
+++ search/search.js
@@ -0,0 +1,10 @@
+periodictable = {
+  list:["H","He","Li","Be","B","C","N","O","F","Ne"],
+  data:[{name:"Hydrogen",symbol:"H",electrons:1,protons:1,neutrons:0}],
+}
+function result(string){
+    
+}
+function atom(str){
+    document.getElementById("specialresult").innerHTML="<h1>"+periodictable.data[periodictable.list.indexOf(str)]+"</h1>"
+}
