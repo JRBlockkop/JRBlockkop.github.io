50f1eaf322edc51a17a728115ef782cb24438ddc
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-26 18:45:36 +0200

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,6 +1,12 @@
 periodictable = {
-  list:["H","He","Li","Be","B","C","N","O","F","Ne"],
-  data:[{name:"Hydrogen",symbol:"H",electrons:1,protons:1,neutrons:0},{name:"Helium",symbol:"He",electrons:2,protons:2,neutrons:2}],
+  list:["Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","N","O","F","Ne"],
+  data:[{name:"Hydrogen",symbol:"H",electrons:1,protons:1,neutrons:0},
+        {name:"Helium",symbol:"He",electrons:2,protons:2,neutrons:2},
+        {name:"Lithium",symbol:"Li",electrons:3,protons:3,neutrons:4},
+        {name:"Beryllium",symbol:"Be",electrons:4,protons:4,neutrons:5},
+        {name:"Boron",symbol:"B",electrons:5,protons:5,neutrons:5},
+        {name:"Carbon",symbol:"C",electrons:6,protons:6,neutrons:6},
+       ],
 }
 function result(string){
     
