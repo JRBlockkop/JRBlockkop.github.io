50466285ae11cf537f80f84746ce1e66bf3a4154
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-26 21:31:32 +0200

    Update search.js

--- search/search.js
+++ search/search.js
@@ -1,11 +1,11 @@
 periodictable = {
   list:["Hydrogen","Helium","Lithium","Beryllium","Boron","Carbon","N","O","F","Ne"],
   data:[{name:"Hydrogen",symbol:"H",electrons:1,protons:1,neutrons:0,mass:1.0078},
-        {name:"Helium",symbol:"He",electrons:2,protons:2,neutrons:2},
-        {name:"Lithium",symbol:"Li",electrons:3,protons:3,neutrons:4},
-        {name:"Beryllium",symbol:"Be",electrons:4,protons:4,neutrons:5},
-        {name:"Boron",symbol:"B",electrons:5,protons:5,neutrons:5},
-        {name:"Carbon",symbol:"C",electrons:6,protons:6,neutrons:6},
+        {name:"Helium",symbol:"He",electrons:2,protons:2,neutrons:2,mass:4.0026},
+        {name:"Lithium",symbol:"Li",electrons:3,protons:3,neutrons:4,mass:6.9410},
+        {name:"Beryllium",symbol:"Be",electrons:4,protons:4,neutrons:5,mass:9.0122},
+        {name:"Boron",symbol:"B",electrons:5,protons:5,neutrons:5,mass:10.811},
+        {name:"Carbon",symbol:"C",electrons:6,protons:6,neutrons:6,mass:12.011},
        ],
 }
 function result(elmtid,href,name,main_title,title,text,keyword){
