71d5433d88643967b787d6ff57dccc59c44dedda
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-26 21:27:28 +0200

    Update search.js

--- search/search.js
+++ search/search.js
@@ -8,8 +8,8 @@ periodictable = {
         {name:"Carbon",symbol:"C",electrons:6,protons:6,neutrons:6},
        ],
 }
-function result(href,name,){
-    
+function result(elmtid,href,name,main_title,title,text,keyword){
+    document.getElementById(elmtid).innerHTML = "<div class='result_title'><div class='top_title'></div>"+title+"</div><div>"+text+"</div>"
 }
 function atom(str){
     document.getElementById("specialresult").innerHTML="<h1>"+periodictable.data[periodictable.list.indexOf(str)].name+"</h1><p><u><b>Symbol:</u></b>"+periodictable.data[periodictable.list.indexOf(str)].symbol+"</p>"+"<p><u><b>Atomic mass:</u></b>"+periodictable.data[periodictable.list.indexOf(str)].mass+"</p>"
