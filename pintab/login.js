6a7a5b17d0ccf01457a008eaf8043bcc953311fb
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-27 22:35:10 +0100

    Update login.js

--- pintab/login.js
+++ pintab/login.js
@@ -1,5 +1,5 @@
 if(document.getElementById("pintab-login-field")!=null){
-    document.getElementById("pintab-login-field").innerHTML = "<iframe src='https://jrblockkop.github.io/pintab/exernlogin.html'></iframe>"
+    document.getElementById("pintab-login-field").innerHTML = "<iframe src='https://jrblockkop.github.io/pintab/externlogin.html'></iframe>"
 }
 else{
   console.error("There is no Pintab login field")
