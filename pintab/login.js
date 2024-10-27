a9a1a4fb23c3970b2d7fee5774300f79bb0c4f22
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-10-27 22:20:35 +0100

    Update login.js

--- pintab/login.js
+++ pintab/login.js
@@ -1,5 +1,5 @@
 if(document.getElementById("pintab-login-field")!=null){
-  
+    document.getElementById("pintab-login-field").innerHTML = "<button>Login with Pintab</button>"
 }
 else{
   console.error("There is no Pintab login field")
