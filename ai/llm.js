c1ce8057dc11241778eb5d3be6f114c47955a94e
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-11-20 20:02:06 +0100

    Update llm.js

--- ai/llm.js
+++ ai/llm.js
@@ -1,23 +1,11 @@
-const sentence = {
-   time_1:"",
-   subject:"",
-   frequencyverb_1:"",
-   verb:"",
-   frequencyverb_2:"",
-   object:"",
-   manner:"",
-   place:"",
-   time_2:"",
-   write:function (){
-      if(time_1=""){
-         return this.subject.charAt(0).toUpperCase()+this.subject.slice(1)
-      }
-   }
+const data = [];
+function WordtoToken(str){
+   console.log(Get)
 }
 let Get;
-async function T(num){
+async function Start(num){
    let x = await fetch("https://jrblockkop.github.io/ai/language.json")
    let y = await x.text()
    Get = JSON.parse(y)
 }
-T()
+Start()
