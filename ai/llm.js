5e0b92a0e6388bee1259e3b5a73bdeef16273f76
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-11-20 20:32:32 +0100

    Update llm.js

--- ai/llm.js
+++ ai/llm.js
@@ -2,6 +2,9 @@ const data = [];
 function WordtoToken(str){
    return Get.data[Get.words.indexOf(str)].v
 }
+function Send(){
+   let Tokens = document.querySelector("textarea").value .split(" ")
+}
 let Get;
 async function Start(num){
    let x = await fetch("https://jrblockkop.github.io/ai/language.json")
