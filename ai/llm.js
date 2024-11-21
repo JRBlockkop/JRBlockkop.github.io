7b48fe9af8f2d349ea40134c96409f022b452e85
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2024-11-21 20:58:33 +0100

    Update llm.js

--- ai/llm.js
+++ ai/llm.js
@@ -2,6 +2,9 @@ const data = [];
 function WordtoToken(str){
    return Get.data[Get.words.indexOf(str)].v
 }
+function TokentoWord(arr){
+   return Get.data[Get.words.indexOf(str)].v
+}
 function Send(){
    let Tokens = document.querySelector("textarea").value.split(" ")
 }
