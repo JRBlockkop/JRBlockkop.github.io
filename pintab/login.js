03232e4802133b6764636b52895951d2b0c36012
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2025-02-22 20:17:09 +0100

    Update login.js

--- pintab/login.js
+++ pintab/login.js
@@ -2,6 +2,9 @@ const Pintab = {
     IsLogin:false,
     route:"/",
     login:()=>{
+        const iframe = document.createElement("iframe");
+        iframe.src = "https://jrblockkop.github.io/pintab/"
+        document.appendChild(iframe);
         href = location.href
         window.open("https://jrblockkop.github.io/pintab/login?r="+href + Pintab.route,"Pintab","popup,left=100,top=100,width=380,height=420")
     },
