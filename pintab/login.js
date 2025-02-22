e58c38328b2552d49167d67d0019b60c0efb5f5a
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2025-02-22 13:22:18 +0100

    Update login.js

--- pintab/login.js
+++ pintab/login.js
@@ -1,3 +1,10 @@
-const Auth = {
-    
+const Pintab = {
+    IsLogin:false,
+    login:()=>{
+        window.open("https://jrblockkop.github.io/pintab/login?r=http://localhost:3030/api/pintab","Pintab","popup,left=100,top=100,width=320,height=320")
+    },
+    User:{
+        name:"",
+        sha:"",
+    },
 }
