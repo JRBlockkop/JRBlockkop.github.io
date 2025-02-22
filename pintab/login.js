0f755f16e2b58f8d34ba3e6284a42457fcca1015
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2025-02-22 19:55:10 +0100

    Update login.js

--- pintab/login.js
+++ pintab/login.js
@@ -1,8 +1,9 @@
 const Pintab = {
     IsLogin:false,
+    route:"/",
     login:()=>{
         href = location.href
-        window.open("https://jrblockkop.github.io/pintab/login?r="+href,"Pintab","popup,left=100,top=100,width=380,height=420")
+        window.open("https://jrblockkop.github.io/pintab/login?r="+href + Pintab.route,"Pintab","popup,left=100,top=100,width=380,height=420")
     },
     User:{
         name:"",
